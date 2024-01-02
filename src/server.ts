ad49b9a67dcd002d1ad37abd64eefbc8b3a32b60
wesjps <wesleyjps@yahoo.com.br>
2024-01-02 10:22:42 -0300

    ✨ feat: Documentação com Swagger

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,39 @@
-import express,{Request,Response,NextFunction} from 'express';
-import {router} from './routes';
-import 'express-async-errors';
+import express, { Request, Response, NextFunction } from "express";
+import { router } from "./routes";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+import "express-async-errors";
+import path from "path";
 
 const app = express();
 const PORT = 3333;
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("/v1", router);
 
-app.use((err: Error, request: Request, response: Response, next: NextFunction)=>{
-    if(err instanceof Error){
-        return response.status(400).json({error: err.message})
-    }
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
-    return response.status(500).json({
-        status: 'error',
-        message: 'Internal server error'
-    })
-})
+app.use(
+	(err: Error, request: Request, response: Response, next: NextFunction) => {
+		if (err instanceof Error) {
+			return response.status(400).json({ error: err.message });
+		}
 
-app.listen(PORT,()=>{
-    console.log(`Servidor rodando na porta ${PORT} - Projeto Inventory Control`)
-})
+		return response.status(500).json({
+			status: "error",
+			message: "Internal server error",
+		});
+	}
+);
+
+app.get("/terms", (request: Request, response: Response) => {
+	return response.json({
+		message: "Termos de serviço",
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Servidor rodando na porta ${PORT} - Projeto Inventory Control`);
+});
